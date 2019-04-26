e3ee85cbcb620446ab66e06d4847da4b093a5ca7
tdx1997tdx <798637048@qq.com>
2019-04-27 02:15:58 +0800

    d

--- op_sql/other_search_sql.py
+++ op_sql/other_search_sql.py
@@ -2,7 +2,7 @@ from op_sql import op_postgresql as opsql
 import op_sql.conn_sql as c
 import random
 def prototype_search(prototype,conn_info=c.get_now_conn()):
-    sql = "select prototype_name,s.servent_id,servent_name,profile_pic from servent s inner join servent_prototype sp on s.servent_id=sp.servent_id inner join prototype p on sp.prototype_id=p.prototype_id inner join servent_profile_pic spp on s.servent_id = spp.servent_id where prototype_name='%s'" % (prototype)
+    sql = "select s.servent_id,servent_name,profile_pic from servent s inner join servent_prototype sp on s.servent_id=sp.servent_id inner join prototype p on sp.prototype_id=p.prototype_id inner join servent_profile_pic spp on s.servent_id = spp.servent_id where prototype_name='%s'" % (prototype)
     result = [i for i in opsql.select(sql, conn_info)]
     dic_info = []  # 所有人
     for row in result:
