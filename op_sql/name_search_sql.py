7a01c78c7be5b051c41b9d19611f9ccfbd0d9671
tdx1997tdx <798637048@qq.com>
2019-05-07 22:20:23 +0800

    u

--- op_sql/name_search_sql.py
+++ op_sql/name_search_sql.py
@@ -6,14 +6,14 @@ def name_search(name,conn_info=c.get_now_conn()):
         search_name += i
         search_name += '%'
     sql="select servent_id,servent_name from servent where servent_name like '%s'"
-    result = [i for i in opsql.select(sql,(search_name),conn_info)]
+    result = [i for i in opsql.select(sql,(search_name,),conn_info)]
     dic_info = []  # 所有人
     for row in result:
         dic_one_person_info = {}  # 单个人
         dic_one_person_info['id'] = str(row[0])
         dic_one_person_info["servent_name"]=row[1]
         temp_sql="select profile_pic from servent_profile_pic where servent_id='%s'"
-        re=[i[0] for i in opsql.select(temp_sql,(row[0]),conn_info)]
+        re=[i[0] for i in opsql.select(temp_sql,(row[0],),conn_info)]
         dic_one_person_info["servent_profile_pic"] = re[0]
         dic_info.append(dic_one_person_info)
     return dic_info
