ef2753dff5fe5bf3db645b8121f09b4b586df2c9
tdx1997tdx <798637048@qq.com>
2019-05-09 01:57:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.register_blueprint(page3)
 app.register_blueprint(page4)
 app.register_blueprint(page5)
 app.register_blueprint(page6)
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/',methods=['GET','POST'])
 def hello_world():
